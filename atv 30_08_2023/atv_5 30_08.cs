77d7f61b21104049842070aec308499de93364a2
nagamilui <142070881+nagamilui@users.noreply.github.com>
2023-08-30 21:34:24 -0300

    Update atv_5 30_08.cs

--- atv 30_08_2023/atv_5 30_08.cs
+++ atv 30_08_2023/atv_5 30_08.cs
@@ -4,16 +4,16 @@ public class atv5
 {
     public static void Main(string[] args)
     {
-        Console.OutputEncoding=Encoding.UTF8;
-        Console.WriteLine ("digite o usuário");
-        string palavra = Console.ReadLine();
-        Console.WriteLine ("digite a senha");
-        int senha = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine ("digite o nome completo");
+        string nomecompleto = Console.ReadLine();
+        Console.WriteLine ("digite a idade");
+        int idade = Convert.ToInt16(Console.ReadLine());
         
-        if(palavra == "admin" && senha == 1234 ){
-            Console.WriteLine("Bem vindo admin");
-        }else{
-            Console.WriteLine("Acesso negado");
-        }
+        Console.WriteLine($"seu nome maiusculo{nomecompleto.ToUpper()}");
+        Console.WriteLine($"seu nome minusculo{nomecompleto.ToLower()}");
+        string nomesemespaco = nomecompleto.Replace(" ", "");
+        Console.WriteLine($"a quantidade de caracteres do seu nome é {nomesemespaco.Length}");
+        int anonascimento = idade - 2023;
+        Console.WriteLine($"seu ano de nascimento{anonascimento} e daqui 100 anos voce terá {idade + 100}");
     }
 }
